6744a0e10362f1ef24c27234b33e629442e1ab4b
Michael Sol <sol.mike.j@gmail.com>
2023-03-12 14:36:27 -0700

    How much setup can one project take

--- HairSalon/Controllers/ClientsController.cs
+++ HairSalon/Controllers/ClientsController.cs
@@ -1,5 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
-using VendorOrderTracker.Models;
+using HairSalon.Models;
 using System.Collections.Generic;
 using System;
 
--- HairSalon/Models/Client.cs
+++ HairSalon/Models/Client.cs
@@ -8,22 +8,6 @@ namespace HairSalon.Models
     public string Name { get; set; }
     public DateTime NextAppointment { get; set; }
     public Stylist PersonalStylist { get; set; }
-    public int ClientId {get;}
-
-    private static List<Client> _instances = new List<Client> { };
-    public Client(string name, DateTime nextAppointment, Stylist personalStylist) 
-    {
-      Name = name;
-      NextAppointment = nextAppointment;
-      PersonalStylist = personalStylist;
-      Date = date;
-      _instances.Add(this);
-      Id = _instances.Count;
-    }
-
-  public static void ClearAll(){
-    _instances.Clear();
-  }
-
+    public int ClientId {get; set;}
   }
 }
