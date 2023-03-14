eb579e1b26f88a78758c72c92be3d35d91f67168
Michael Sol <sol.mike.j@gmail.com>
2023-03-13 21:27:50 -0700

    Fixed create for clients

--- HairSalon/Models/HairSalonContext.cs
+++ HairSalon/Models/HairSalonContext.cs
@@ -4,7 +4,6 @@ namespace HairSalon.Models{
     public class HairSalonContext : DbContext{
         public DbSet<Client> Clients { get; set; }
         public DbSet<Stylist> Stylists {get; set; }
-
         public HairSalonContext(DbContextOptions options) : base(options) { }
     }
 }
